ad794b81aed23490c4109d828756f7479b7ef162
otaviocomp <otavio10espsanto@gmail.com>
2017-12-19 20:02:55 -0300

    update

--- gradiente.c
+++ gradiente.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *deriv(int *coef, int *dcoef, int n);
+//void input_vars(int n, float *coef, float *sted, float *start);
+float *deriv(int n, float *coef, float *dcoef);
 
 int main()
 {
 	int n, i;
-	float step, init;
+	float step, start;
 	printf("digite a ordem da funcao: ");
 	scanf("%d", &n);
 		
-	// alocar vetores com coeficientes da funcao
-	int *coef = malloc(n*sizeof(int));    // f(x)
-	int *dcoef = malloc((n - 1)*sizeof(int));    // f'(x)	
-	
+	// alocar vetores com coeficientes da funcao // coef -> f(x) dcoef -> f'(x)
+	float *coef = malloc((n + 1)*sizeof(float));    
+	float *dcoef = malloc(n*sizeof(float));    
+
 	printf("digite os coeficientes da funcao (do coeficiente de maior ordem para o menor):\n");
-	for(i = n; i >= 0; i--)
-		scanf("%d", &coef[i]);
-
+	for(i = 0; i <= n; i++)
+	{
+		printf("coeficiente de x^%d = ", n - i);
+		scanf("%f", &coef[i]);
+	}
 	printf("digite o valor do passo: ");
-	scanf("%f", step);
+	scanf("%f", &step);
 	printf("digite o valor inicial: ");
-	scanf("%f", init);
+	scanf("%f", &start);
 
-	dcoef = deriv(coef, dcoef, n);	
+	//input_vars(n, coef, &step, &start);
+	dcoef = deriv(n, coef, dcoef);	
+
+	printf("f(x) = ");
+	for(i = 0; i <= n; i++)
+		printf("%fx^%d ", coef[i], n - i);
+	printf("\n");
+	printf("f'(x) = ");
+	for(i = 0; i < n; i++)
+		printf("%fx^%d ",dcoef[i], n - i - 1);
+	printf("\nstep = %f start = %f\n", step, start);	
+	printf("\n");	
 }
 
-int *deriv(int *coef, int *dcoef, int n)
+/*void input_vars(int n, float *coef, float *step, float *start)
+{
+
+}
+*/
+float *deriv(int n, float *coef, float *dcoef)
 {
 	int i;
-	for(i = n - 1; i >= 0; i++)
-		dcoef[i] = coef[i + 1]*(i + 1);
+	for(i = 0; i < n; i++)
+		dcoef[i] = coef[i]*(n - i);
 	return dcoef;
 }
-
 
