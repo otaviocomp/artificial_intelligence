a69ff3c9afea9879ed4b86db44f793477344d71e
otaviocomp <otavio10espsanto@gmail.com>
2018-03-25 18:06:43 -0300

    update minmax

--- jogodavelha/minmax.c
+++ jogodavelha/minmax.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-char gridChar(int i){
+char tabu(int i){
     switch(i){
         case -1:
             return 'X';
@@ -12,18 +12,18 @@ char gridChar(int i){
 }
 
 void draw(int b[9]){
-    printf(" %c | %c | %c\n",gridChar(b[0]),gridChar(b[1]),gridChar(b[2]));
+    printf(" %c | %c | %c\n",tabu(b[0]),tabu(b[1]),tabu(b[2]));
     printf("---+---+---\n");
-    printf(" %c | %c | %c\n",gridChar(b[3]),gridChar(b[4]),gridChar(b[5]));
+    printf(" %c | %c | %c\n",tabu(b[3]),tabu(b[4]),tabu(b[5]));
     printf("---+---+---\n");
-    printf(" %c | %c | %c\n",gridChar(b[6]),gridChar(b[7]),gridChar(b[8]));
+    printf(" %c | %c | %c\n",tabu(b[6]),tabu(b[7]),tabu(b[8]));
 }
 
 int win(const int board[9]){
     //determines if a player has won, returns 0 otherwise.
     unsigned wins[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int i;
-    for(i = 0; i < 8; ++i){
+    for(i=0; i<8; i++){
         if(board[wins[i][0]] != 0 &&
            board[wins[i][0]] == board[wins[i][1]] &&
            board[wins[i][0]] == board[wins[i][2]])
@@ -32,58 +32,59 @@ int win(const int board[9]){
     return 0;
 }
 
-int minimax(int board[9], int player){
+int minmax(int board[9], int player){
     //How is the position like for player (their turn) on board?
     int winner = win(board);
     if(winner != 0) 
 		return winner*player;
-
-    int move = -1;
-    int score = -2;//Losing moves are preferred to no move
+    int move=-1;
+    int score=-2;//Losing moves are preferred to no move
     int i;
     for(i=0; i<9; i++){//For all moves,
         if(board[i] == 0){//If legal,
             board[i] = player;//Try the move
-            int thisScore = -minimax(board, player*-1);
-            if(thisScore > score) {
+            int thisScore = -minmax(board, -player);
+            if(thisScore > score){
                 score = thisScore;
                 move = i;
             }//Pick the one that's worst for the opponent
             board[i] = 0;//Reset board after try
         }
     }
-    if(move == -1) 
+    if(move==-1) 
 		return 0;
     return score;
 }
 
 void computerMove(int board[9]){
-    int move=-1;
-    int score = -2;
-    int i;
-    for(i=0; i<9; i++){
+    int move=-1, score=-10, i;
+    for(i=0; i<9; i++)
         if(board[i]==0){
             board[i]=1;
-            int tempScore = -minimax(board,-1);
+            int tempScore = -minmax(board,-1);
             board[i]=0;
             if(tempScore > score){
                 score = tempScore;
                 move = i;
             }
-        }
-    }
-    //returns a score based on minimax tree at a given node.
+    	}
+    //returns a score based on minmax tree at a given node.
     board[move] = 1;
 }
 
 void playerMove(int board[9]){
-    int move = 0;
-    do{
-        printf("\nInput move ([0..8]): ");
+    int move;
+	int flag=1;
+    while(flag){
+        printf("\nmovement(value between 0 and 8): ");
         scanf("%d", &move);
         printf("\n");
-    } while (move >= 9 || move < 0 && board[move] == 0);
-    board[move] = -1;
+		if(move>8 || move<0 || board[move]!=0)
+			flag=1;
+		else
+			flag=0;
+	}		
+    board[move]=-1;
 }
 
 int main(){
@@ -104,14 +105,14 @@ int main(){
     }
     switch(win(board)){
         case 0:
-            printf("A draw. How droll.\n");
+            printf("Draw\n");
             break;
         case 1:
             draw(board);
             printf("You lose.\n");
             break;
         case -1:
-            printf("You win. Inconceivable!\n");
+            printf("You win. Congratulations!\n");
             break;
     }
 }
