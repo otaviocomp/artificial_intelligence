10f4b4b49f931522d3e12b220b8323fd04469973
otaviocomp <otavio10espsanto@gmail.com>
2018-04-07 15:58:44 -0300

    update

--- jogodavelha/minmax.cpp
+++ jogodavelha/minmax.cpp
@@ -12,19 +12,51 @@ Move Ai::Search(int player){
         case TIE:
             return TIE_SCORE;
     }
-    Move bestMove;
-    bestMove.score = -INF; //begin with a very low value
-    for(int i = 0; i < 9; i++){
-        if(board.GetSquare(i) == EMPTY){
-            Move move;
-            move.location = i;
-            board.SetSquare(i, player);
-            move.score = -Search(-player).score;
-            board.UnsetSquare(i);
-            // att the score
-            if(move.score > bestMove.score)
-                bestMove = move;
+    bool easy=0;
+    if(easy==1){
+        Move bestMove;
+        if(player==-1){
+            bestMove.score = -INF;
+            for(int i = 0; i < 9; i++){
+                if(board.GetSquare(i) == EMPTY){
+                    Move move;
+                    move.location = i;
+                    board.SetSquare(i, player);
+                    board.UnsetSquare(i);
+                    if(move.score > bestMove.score)
+                        bestMove = move;
+                }
+            }
+            return bestMove;
+        }
+        else{
+            bestMove.score = INF;
+            for(int i = 0; i < 9; i++){
+                if(board.GetSquare(i) == EMPTY){
+                    Move move;
+                    move.location = i;
+                    board.SetSquare(i, player);
+                    board.UnsetSquare(i);
+                    if(move.score < bestMove.score)
+                        bestMove = move;
+                }
+            }
+            return bestMove;
         }
     }
-    return bestMove;
+    else{
+        Move bestMove;
+        bestMove.score = -INF;
+        for(int i = 0; i < 9; i++)
+            if(board.GetSquare(i) == EMPTY){
+                Move move;
+                move.location = i;
+                board.SetSquare(i, player);
+                move.score = -Search(-player).score;
+                board.UnsetSquare(i);
+                if(move.score > bestMove.score)
+                    bestMove = move;
+            }
+        return bestMove;
+    }
 }
