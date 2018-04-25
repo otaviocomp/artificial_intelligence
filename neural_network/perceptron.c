2c7e592aeadbd9327de5dd4d88bd6809b5b76c5c
otaviocomp <otavio10espsanto@gmail.com>
2018-04-25 21:10:05 +0000

    update perceptron

--- neural_network/perceptron.c
+++ neural_network/perceptron.c
@@ -2,7 +2,7 @@
 
 int main(){
 	float soma, pesos[3] = {0,0,0};
-	int erro, i, j, input1, input2, epocas, resposta;
+	int erro, j, input1, input2, epocas, resposta;
 	const int dados[4][3] = {{0,0,0},{0,1,0},{1,0,0},{1,1,1}};
 	
 	//epocas
@@ -10,27 +10,28 @@ int main(){
 	scanf("%d", &epocas);
 
 	//treinamento
-	for(i = epocas; i--;)
+	for(epocas; epocas--;)
 		for(j = 4; j--;){
+			printf("%d\n", epocas);
 			//soma pesos sinapticos e inputs
 			soma = 0;
-			soma = -1*pesos[0] + dados[4 - j][0]*pesos[1] + dados[4 - j][1]*pesos[2];
+			soma = 1*pesos[0] + dados[3 - j][0]*pesos[1] + dados[3 - j][1]*pesos[2];
 
 			//funcao de ativacao (degrau)
 			if(soma > 0) resposta = 1;
 			else resposta = 0;
 
 			//calcula o erro
-			erro = dados[4 - j][2] - resposta;
+			erro = dados[3 - j][2] - resposta;
 
 			//atualiza os pesos
 			if(erro){
-				pesos[0] = pesos[0] + 0.5*erro*(-1);				
+				pesos[0] = pesos[0] + 1*erro*(1);				
 				printf("erro: %d - input: %d - peso: %f\n", erro, -1, pesos[0]); 
-				pesos[0] = pesos[0] + 0.5*erro*dados[4 - j][0];
-				printf("erro: %d - input: %d - peso: %f\n", erro, dados[4 - j][0], pesos[1]); 
-				pesos[0] = pesos[0] + 0.5*erro*dados[4 - j][1];
-				printf("erro: %d - input: %d - peso: %f\n", erro, dados[4 - j][1], pesos[2]); 
+				pesos[0] = pesos[0] + 0.5*erro*dados[3 - j][0];
+				printf("erro: %d - input: %d - peso: %f\n", erro, dados[3 - j][0], pesos[1]); 
+				pesos[0] = pesos[0] + 0.5*erro*dados[3 - j][1];
+				printf("erro: %d - input: %d - peso: %f\n", erro, dados[3 - j][1], pesos[2]); 
 			}
 		}	
 	puts("RESULTADO DO TREINAMENTO:");		
@@ -48,7 +49,7 @@ int main(){
 
 		//resultado
 		soma = 0;
-		soma = -1*pesos[0] + input1*pesos[1] + input2*pesos[2];
+		soma = 1*pesos[0] + input1*pesos[1] + input2*pesos[2];
 		if(soma > 0) resposta = 1;
 		else resposta = 0;
 		printf("\nRESULTADO: %d\n", resposta);
