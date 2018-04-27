5ec7c0529c26f0a5ab5ffe586570309ab7db8383
otaviocomp <otavio10espsanto@gmail.com>
2018-04-27 19:20:05 +0000

    multilayer perceptron

--- neural_network/multilayerPerceptron.c
+++ neural_network/multilayerPerceptron.c
@@ -5,12 +5,12 @@
 
 //Bias = 1 and learning rate = 1;
 
-float sumHidden(float pesos[][3], const int dados[][3], int i, int j){
-	return pesos[i][0] + pesos[i][1]*dados[j][0] + pesos[i][2]*dados[j][1];
+float sumHidden(float weight[][3], const int data[][3], int i, int j){
+	return weight[i][0] + weight[i][1]*data[j][0] + weight[i][2]*data[j][1];
 }
 
-float sumOutput(float pesos[], float h1, float h2){
-	return pesos[0] + pesos[1]*h1 + pesos[2]*h2;
+float sumOutput(float weight[], float h1, float h2){
+	return weight[0] + weight[1]*h1 + weight[2]*h2;
 }
 
 float sigmoide(float x){
@@ -25,59 +25,61 @@ float errorOutput(float d, float y){
 	return (d - y)*derivSigmoide(y);
 }
 
-float errorHidden(float deltaOutput, float pesosOutput[], float y){
-	return derivSigmoide(y)*(deltaOutput*pesosOutput[0] + deltaOutput*pesosOutput[1] + deltaOutput*pesosOutput[2]);
+float errorHidden(float deltaOutput, float weightOutput[], float y){
+	return derivSigmoide(y)*(deltaOutput*weightOutput[0] + deltaOutput*weightOutput[1] + deltaOutput*weightOutput[2]);
 }
 
-void training(float pesosHidden[][3], float pesosOutput[], const int dados[][3], int epochs){
+void training(float weightHidden[][3], float weightOutput[], const int data[][3], int epochs){
 	int i;
 	float h1, h2, y, e, deltaOutput, deltaHidden;
 	for(epochs; epochs--;)
 		for(i = 4; i--;){
 			//feedForward
-			h1 = sigmoide(sumHidden(pesosHidden, dados, 0, 3 - i)); //output hidden neuron 1
-			h2 = sigmoide(sumHidden(pesosHidden, dados, 1, 3 - i)); //output hidden neuron 2
-			y = sigmoide(sumOutput(pesosOutput, h1, h2)); //output neuron last layer
-			e = dados[3 - i][2] - y;
+			h1 = sigmoide(sumHidden(weightHidden, data, 0, 3 - i)); //output hidden neuron 1
+			h2 = sigmoide(sumHidden(weightHidden, data, 1, 3 - i)); //output hidden neuron 2
+			y = sigmoide(sumOutput(weightOutput, h1, h2)); //output neuron last layer
+			e = data[3 - i][2] - y;
+			if(e < 0) -1*e;
+			printf("erro: %f\n", e);
 			if(e > MAX_E){ //backPropagation 
 				//output layer
-				deltaOutput = errorOutput(dados[3 - i][2], y);
-				pesosOutput[0] = pesosOutput[0] + deltaOutput;
-				pesosOutput[1] = pesosOutput[1] + deltaOutput*h1;
-				pesosOutput[2] = pesosOutput[2] + deltaOutput*h2;
+				deltaOutput = errorOutput(data[3 - i][2], y);
+				weightOutput[0] = weightOutput[0] + deltaOutput;
+				weightOutput[1] = weightOutput[1] + deltaOutput*h1;
+				weightOutput[2] = weightOutput[2] + deltaOutput*h2;
 
 				//hidden layer
-				deltaHidden = errorHidden(deltaOutput, pesosOutput, y);
-				pesosHidden[0][0] = pesosHidden[0][0] + deltaHidden;
-				pesosHidden[0][1] = pesosHidden[0][1] + deltaHidden*dados[3 - i][0];
-				pesosHidden[0][2] = pesosHidden[0][2] + deltaHidden*dados[3 - i][1];
-				pesosHidden[1][0] = pesosHidden[1][0] + deltaHidden;
-				pesosHidden[1][1] = pesosHidden[1][1] + deltaHidden*dados[3 - i][0];
-				pesosHidden[1][2] = pesosHidden[1][2] + deltaHidden*dados[3 - i][1];
+				deltaHidden = errorHidden(deltaOutput, weightOutput, y);
+				weightHidden[0][0] = weightHidden[0][0] + deltaHidden;
+				weightHidden[0][1] = weightHidden[0][1] + deltaHidden*data[3 - i][0];
+				weightHidden[0][2] = weightHidden[0][2] + deltaHidden*data[3 - i][1];
+				weightHidden[1][0] = weightHidden[1][0] + deltaHidden;
+				weightHidden[1][1] = weightHidden[1][1] + deltaHidden*data[3 - i][0];
+				weightHidden[1][2] = weightHidden[1][2] + deltaHidden*data[3 - i][1];
 			}
 		}		
 }
 
 int main(){
-	int epochs, input1, input2;
-	float result, h1, h2, pesosHidden[2][3] = {0,0,0,0,0,0}, pesosOutput[3] = {0,0,0};
-	const int dados[4][3] = {{0,0,0},{1,0,1},{0,1,1},{1,1,0}};	//XOR port
+	int epochs, input1, input2, result;
+	float h1, h2, weightHidden[2][3] = {0,0,0,0,0,0}, weightOutput[3] = {0,0,0};
+	const int data[4][3] = {{0,0,0},{1,0,1},{0,1,1},{1,1,0}};	//XOR port
 
 	fputs("epochs:", stdout);
-	scanf("%d", epochs);
-	training(pesosHidden, pesosOutput, dados, epochs);
+	scanf("%d", &epochs);
+	training(weightHidden, weightOutput, data, epochs);
 	while(1){
 		//inputs 
 		fputs("input1: ", stdout);
-		scanf("%f", input1);
+		scanf("%d", &input1);
 		fputs("input2: ", stdout);
-		scanf("%f", input2);
+		scanf("%d", &input2);
 
-		h1 = sigmoide(pesosHidden[0][0] + pesosHidden[0][1]*input1 + pesosHidden[0][2]*input2);
-		h2 = sigmoide(pesosHidden[1][0] + pesosHidden[1][1]*input1 + pesosHidden[1][2]*input2);
-		result = sigmoide(pesosOutput[0] + pesosOutput[1]*h1 + pesosOutput[2]*h2);
+		h1 = sigmoide(weightHidden[0][0] + weightHidden[0][1]*input1 + weightHidden[0][2]*input2);
+		h2 = sigmoide(weightHidden[1][0] + weightHidden[1][1]*input1 + weightHidden[1][2]*input2);
+		result = (int)sigmoide(weightOutput[0] + weightOutput[1]*h1 + weightOutput[2]*h2);
 
 		fputs("RESULT:", stdout);
-		printf("%f", result);
+		printf("%d\n", result);
 	}	
 }
